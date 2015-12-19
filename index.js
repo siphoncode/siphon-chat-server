d1e8c06c391448074ec56981fc507e9346e7a877
James Potter <james.potter@gmail.com>
2015-12-19 18:36:02 +0700

    Send pings to prevent idle timeout.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var WebSocketServer = require("ws").Server;
-var http = require("http");
-var express = require("express");
+var WebSocketServer = require('ws').Server;
+var http = require('http');
+var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + "/"));
+app.use(express.static(__dirname + '/'));
 
 var server = http.createServer(app);
 server.listen(port);
@@ -21,10 +21,21 @@ wss.broadcast = function(data) {
 
 var currentID = 0;
 
+var pings = function(ws) {
+  setTimeout(function() {
+    if (ws.readyState != 1) return; // socket closed, stop sending pings
+    ws.send('ping');
+    pings(ws);
+  }, 2000);
+};
+
 wss.on('connection', function(ws) {
   ws.id = currentID++; // give the user a sort-of unique ID
   console.log('New connection from #' + ws.id);
   ws.on('message', function(message) {
     wss.broadcast('[user-' + ws.id + '] ' + message);
   });
+
+  // Prevent idle timeouts
+  pings(ws);
 });
